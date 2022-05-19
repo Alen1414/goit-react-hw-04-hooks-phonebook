0f6b5f5a7d8eab68bbe127de1ad53e4b28688fae
Alona Puhach <alen1414@gmail.com>
2022-05-19 15:09:30 -0400

    huksForm

--- src/components/FormOld.js
+++ src/components/FormOld.js
@@ -1,59 +1,61 @@
-import  React, { useState } from 'react';
+import React, { Component } from 'react';
 import 'components/Form.css';
-
-export default function Form() {
-const [name, setName] = useState('');
-const [number, setNumber] = useState('');
-
-  //-------- получаем что вводим в инпуте
- 
-   const hendelChange = e => {
-  setName(e.currentTarget.value);
-   };
-  
-  const hendelChangeNumber = e => {
-  setNumber(e.currentTarget.value);
-  };
-  
-  const hendelSubmit = e => {
-    e.preventDefault();
-    console.log(name, number);
-    // this.props.onSubmit(this.state);
-    // this.reset();
-  };
-   
+class Form extends Component{
+    state = {
+        name: '',
+        number: '',
+    };
     
-  return (
+
+   //-------- получаем что вводим в инпуте
+    hendelChange = e => {
+    const { name, value } = e.currentTarget;
+    this.setState({ [name]: value });
+    };
+    hendelSubmit = e => {
+    e.preventDefault();
+    // console.log(this.state);
+    this.props.onSubmit(this.state);
+    this.reset();
+    };
+    //-----очищаем инпут после отправки 
+    
+  reset = () => {
+    this.setState({ name: '', number: '' });
+  };
+    render() {
+        return (
             
-            <form className='main_form' onSubmit={hendelSubmit}>
+            <form className='main_form' onSubmit={this.hendelSubmit}>
                 
           <label className='main_input'>
             Name
             <input
-              value={name}
+              value={this.state.name}
               type="text"
               name="name"
               pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
               title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
               required
-              onChange={hendelChange}
+              onChange={this.hendelChange}
              
             />
           </label>
           <label className='main_input' >
             Number
             <input
-              value={number}
+              value={this.state.number}
               type="tel"
               name="number"
               pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
               title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
               required
-              onChange={hendelChangeNumber}
+              onChange={this.hendelChange}
              
             />
           </label>
           <button className='main_buttom' type="submit">Add contact</button>
         </form>);
     }
-
+}
+export default Form;
